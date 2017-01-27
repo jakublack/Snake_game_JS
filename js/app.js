9723376c0fbcfd490b182771bf5aef6e2e767653
Jakub Gawron <jakub.gawron@me.com>
2017-01-27 21:16:13 +0100

    Add some clean code

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 var canvas = document.getElementById('snakeArea');
+var btnStart = document.querySelector(".btn");
 var ctx = canvas.getContext('2d');
 var snakeSize = 15; 
 var w = window.innerWidth -10;
@@ -11,142 +12,140 @@ canvas.setAttribute("height",h);
 
 var allDraw = (function () { 
 
-  var lineSnake = function(x, y) {
+    var lineSnake = function(x, y) {
         ctx.fillStyle = 'white';
         ctx.fillRect(x*snakeSize, y*snakeSize, snakeSize, snakeSize);
-  }
+    }
 
-  var foodToEat = function(x, y) {
+    var foodToEat = function(x, y) {
         ctx.fillStyle = 'blue';
         ctx.fillRect(x*snakeSize, y*snakeSize, snakeSize, snakeSize);
-  }
-  var startSnake = function() {
-      var length = 5;
-      snake = [];
-      for (var i = length-1; i>=0; i--) {
-          snake.push({x:i, y:0});
-      }  
-  }
+    }
+    var startSnake = function() {
+        var length = 5;
+        snake = [];
+        for (var i = length-1; i>=0; i--) {
+            snake.push({x:i, y:0});
+        }  
+    }
     
-  var mainDraw = function(){
-      //bg black
-      ctx.fillStyle = 'black';
-      ctx.fillRect(0, 0, w, h);
+    var mainDraw = function(){
+        //bg black
+        ctx.fillStyle = 'black';
+        ctx.fillRect(0, 0, w, h);
       
-      var snakeX = snake[0].x;
-      var snakeY = snake[0].y;
+        var snakeX = snake[0].x;
+        var snakeY = snake[0].y;
 
-      if (direction == 'right') { 
-        snakeX++; }
-      else if (direction == 'left') { 
-        snakeX--; }
-      else if (direction == 'up') { 
-        snakeY--; 
-      } else if(direction == 'down') { 
-        snakeY++; }
-      
-      
-      //game over
-      if (snakeX == -1 || snakeX == w/snakeSize || snakeY == -1 || snakeY == h/snakeSize || hitSnake(snakeX, snakeY, snake)) {
-          //restart game
-          ctx.clearRect(0,0,w,h);
-          gameloop = clearInterval(gameloop);
-          return;          
+        if (direction == 'right') { 
+            snakeX++; 
         }
-        
+        else if (direction == 'left') { 
+            snakeX--; 
+        }
+        else if (direction == 'up') { 
+            snakeY--; 
+        } 
+        else if(direction == 'down') { 
+            snakeY++; 
+        }
+        //game over
+        if (snakeX == -1 || snakeX == w/snakeSize || snakeY == -1 || snakeY == h/snakeSize || hitSnake(snakeX, snakeY, snake)) {
+            //restart game
+            btnStart.classList.toggle("hide");
+            ctx.clearRect(0,0,w,h);
+            gameloop = clearInterval(gameloop);
+            return;          
+        }
+
         if(snakeX == food.x && snakeY == food.y) {
-          var tail = {x: snakeX, y: snakeY}; //Create a new head instead of moving the tail
-          
-          //Create new food
-          createFood(); 
-        } else {
-          var tail = snake.pop(); 
-          tail.x = snakeX; 
-          tail.y = snakeY;
+            var tail = {x: snakeX, y: snakeY}; //Create a new head instead of moving the tail
+              //Create new food
+              createFood(); 
+        } 
+        else {
+            var tail = snake.pop(); 
+            tail.x = snakeX; 
+            tail.y = snakeY;
         }
-        
-        snake.unshift(tail); 
+
+            snake.unshift(tail); 
 
         for(var i = 0; i < snake.length; i++) {
-          lineSnake(snake[i].x, snake[i].y);
+            lineSnake(snake[i].x, snake[i].y);
         } 
-        
+
         foodToEat(food.x, food.y); 
-        
-  }
+    }
 
-  var createFood = function() {
-      food = {
-        x: Math.floor((Math.random() * 30) + 1),
-        y: Math.floor((Math.random() * 30) + 1)
-      }
-
-      for (var i=0; i>snake.length; i++) {
-        var snakeX = snake[i].x;
-        var snakeY = snake[i].y;
-      
-        if (food.x===snakeX && food.y === snakeY || food.y === snakeY && food.x===snakeX) {
-          food.x = Math.floor(Math.random + 1);
-          food.y = Math.floor(Math.random + 1);
+    var createFood = function() {
+        food = {
+            x: Math.floor((Math.random() * 30) + 1),
+            y: Math.floor((Math.random() * 30) + 1)
         }
+
+        for (var i=0; i>snake.length; i++) {
+            var snakeX = snake[i].x;
+            var snakeY = snake[i].y;
+
+            if (food.x===snakeX && food.y === snakeY || food.y === snakeY && food.x===snakeX) {
+                food.x = Math.floor(Math.random + 1);
+                food.y = Math.floor(Math.random + 1);
+            }
+        }
+    }
+
+      var hitSnake = function(x, y, array) {
+          for(var i = 0; i < array.length; i++) {
+            if(array[i].x === x && array[i].y === y)
+            return true;
+          } 
+          return false;
       }
-  }
 
-  var hitSnake = function(x, y, array) {
-      for(var i = 0; i < array.length; i++) {
-        if(array[i].x === x && array[i].y === y)
-        return true;
-      } 
-      return false;
-  }
-
-  var init = function(){
-      direction = 'down';
-      startSnake();
-      createFood();
-      gameloop = setInterval(mainDraw, 100);
-  }
-    return {
-      init : init
-    };
-
-    
+      var init = function(){
+          direction = 'down';
+          startSnake();
+          createFood();
+          gameloop = setInterval(mainDraw, 100);
+      }
+      return {
+          init : init
+      }; 
 }());
-    var btn = document.querySelector('.start');
 
-    btn.addEventListener("click", function(){ 
-        allDraw.init();
-    });
+var btn = document.querySelector('.start');
+btn.addEventListener("click", function(){ 
+    btnStart.classList.toggle("hide");
+    allDraw.init();
+});
+document.onkeydown = function(event) {
+    keyCode = window.event.keyCode; 
+    keyCode = event.keyCode;
 
-	document.onkeydown = function(event) {
-
-        keyCode = window.event.keyCode; 
-        keyCode = event.keyCode;
-
-        switch(keyCode) {
-        
+    switch(keyCode) {
         case 37: 
-          if (direction != 'right') {
-            direction = 'left';
-          }
-          break;
+            if (direction != 'right') {
+                direction = 'left';
+            }
+            break;
 
         case 39:
-          if (direction != 'left') {
-          direction = 'right';
-          }
-          break;
+            if (direction != 'left') {
+                direction = 'right';
+            }
+            break;
 
         case 38:
-          if (direction != 'down') {
-          direction = 'up';
-          }
-          break;
+            if (direction != 'down') {
+                direction = 'up';
+            }
+            break;
 
         case 40:
-          if (direction != 'up') {
-          direction = 'down';
-          }
-          break;
-          }
-      }
+            if (direction != 'up') {
+                direction = 'down';
+            }
+            break;
+    }
+}
